d15f489ec778851063f13921c104ac1e700de00f
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-19 22:44:43 +0100

    generate apollo kotlin models and add apollo coroutines dependencies

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -4,9 +4,17 @@ plugins {
     kotlin(module = "android")
 }
 
+apollo {
+    generateKotlinModels.set(true)
+}
+
 dependencies {
     implementation(Dependencies.Kotlin.STDLIB)
     implementation(Dependencies.Apollo.RUNTIME)
+    implementation(Dependencies.Apollo.APOLLO_COROUTINES)
+    implementation(Dependencies.Coroutines.CORE)
+
+    addComposeDependencies()
 
     testImplementation(Dependencies.JUnit.JUNIT)
 }
