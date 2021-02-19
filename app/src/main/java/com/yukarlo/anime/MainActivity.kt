6d40ce814a935ffb75efedd36634f5a9bab1db63
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-19 20:14:55 +0100

    set content Greeting from the feature module

--- app/src/main/java/com/yukarlo/anime/MainActivity.kt
+++ app/src/main/java/com/yukarlo/anime/MainActivity.kt
@@ -5,28 +5,22 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.yukarlo.anime.ui.theme.AnimeTheme
+import com.yukarlo.feature.anime.home.Greeting
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AnimeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
