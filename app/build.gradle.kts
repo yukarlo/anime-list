cf4201b72f948180b983e7bfd4417f5c5ebf686a
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-23 18:55:40 +0100

    add lib-anime module to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 
 dependencies {
     implementation(project(":feature-anime-home"))
+    implementation(project(":lib-anime"))
     implementation(project(":common-android"))
     implementation(project(":remote"))
 
