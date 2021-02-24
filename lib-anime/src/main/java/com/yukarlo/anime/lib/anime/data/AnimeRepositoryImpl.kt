3aa6e3a83db90218091f0944b9e0e852ad2e2381
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-24 17:32:01 +0100

    adjust fetch anime logic to add new params

--- lib-anime/src/main/java/com/yukarlo/anime/lib/anime/data/AnimeRepositoryImpl.kt
+++ lib-anime/src/main/java/com/yukarlo/anime/lib/anime/data/AnimeRepositoryImpl.kt
@@ -4,6 +4,8 @@ import com.yukarlo.anime.core.model.Anime
 import com.yukarlo.anime.core.model.AnimeParam
 import com.yukarlo.anime.lib.anime.domain.AnimeRepository
 import com.yukarlo.anime.lib.anime.mapper.AnimeMapper
+import com.yukarlo.anime.lib.anime.mapper.SeasonMapper
+import com.yukarlo.anime.lib.anime.mapper.SortMapper
 import com.yukarlo.remote.RemoteClient
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -11,12 +13,20 @@ import javax.inject.Inject
 
 internal class AnimeRepositoryImpl @Inject constructor(
     private val remoteClient: RemoteClient,
-    private val mapper: AnimeMapper
+    private val mapper: AnimeMapper,
+    private val seasonMapper: SeasonMapper,
+    private val sortMapper: SortMapper
 ) : AnimeRepository {
 
-    override suspend fun fetchTopAnime(param: TopAnimeParam): Flow<List<Anime>?> =
-        remoteClient.getTopAnimeFlow(page = param.page, itemsPerPage = param.itemsPerPage)
+    override suspend fun fetchAnime(param: AnimeParam): Flow<List<Anime>?> =
+        remoteClient.getTopAnimeFlow(
+            page = param.page,
+            itemsPerPage = param.itemsPerPage,
+            year = param.year,
+            season = seasonMapper.mapSeason(animeSeason = param.season),
+            sort = sortMapper.mapSort(animeSort = param.sort)
+        )
             .map {
-                mapper.mapToDomain(result = it.media)
+                mapper.mapTopAnimeToDomain(result = it.media)
             }
 }
