004b40f3d613a7696eabf2c82ae03d75b689eef4
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-24 17:26:45 +0100

    add new data class for anime - season and sort

---
+++ core-model/src/main/java/com/yukarlo/anime/core/model/AnimeSeason.kt
@@ -0,0 +1,8 @@
+package com.yukarlo.anime.core.model
+
+enum class AnimeSeason {
+    WINTER,
+    SPRING,
+    SUMMER,
+    FALL
+}
---
+++ core-model/src/main/java/com/yukarlo/anime/core/model/AnimeSort.kt
@@ -0,0 +1,7 @@
+package com.yukarlo.anime.core.model
+
+enum class AnimeSort {
+    POPULARITY_DESC, // for all time popular
+    SCORE_DESC, // for all time top
+    TRENDING_DESC // for trending
+}
