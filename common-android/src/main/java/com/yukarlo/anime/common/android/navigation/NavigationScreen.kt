35d37eb9ca5e85417af1cd214c993792cc2e8fbd
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-28 18:57:08 +0100

    add parcelableKey prop in NavigationScreens

--- common-android/src/main/java/com/yukarlo/anime/common/android/navigation/NavigationScreen.kt
+++ common-android/src/main/java/com/yukarlo/anime/common/android/navigation/NavigationScreen.kt
@@ -1,15 +1,18 @@
 package com.yukarlo.anime.common.android.navigation
 
 sealed class NavigationScreens(
+    val parcelableKey: String,
     val route: String,
     val label: String,
 ) {
     object AnimeDetails : NavigationScreens(
+        parcelableKey = "",
         route = "AnimeDetails",
         label = "Anime Details"
     )
 
     object ViewAllAnime : NavigationScreens(
+        parcelableKey = "AnimeInputModelKey",
         route = "ViewAllAnime",
         label = "All Anime"
     )
