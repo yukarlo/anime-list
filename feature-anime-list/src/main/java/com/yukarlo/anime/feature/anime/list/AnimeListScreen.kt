1f9b69c4060b034b0f47dcb6aab59c24545c5615
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-28 18:56:28 +0100

    add initial anime list screen

---
+++ feature-anime-list/src/main/java/com/yukarlo/anime/feature/anime/list/AnimeListScreen.kt
@@ -0,0 +1,15 @@
+package com.yukarlo.anime.feature.anime.list
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import com.yukarlo.anime.common.android.navigation.AnimeInputModel
+
+@Composable
+fun AnimeListScreen(
+    navBackStackEntry: NavBackStackEntry,
+    navController: NavController,
+    parcelable: AnimeInputModel?
+) {
+
+}
