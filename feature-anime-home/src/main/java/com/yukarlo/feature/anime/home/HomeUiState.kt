070912b63c1883e3dad286ef90765f90626f8838
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-01 17:34:22 +0100

    adjust home ui state for home items

--- feature-anime-home/src/main/java/com/yukarlo/feature/anime/home/HomeUiState.kt
+++ feature-anime-home/src/main/java/com/yukarlo/feature/anime/home/HomeUiState.kt
@@ -1,8 +1,10 @@
 package com.yukarlo.feature.anime.home
 
 import com.yukarlo.anime.common.android.base.Result
+import com.yukarlo.anime.core.model.Anime
+import com.yukarlo.anime.core.model.AnimeSortTypes
 
 internal data class HomeUiState(
     val result: Result = Result.LOADING,
-    val homeItems: List<HomeItems> = emptyList()
+    val homeItems: LinkedHashMap<AnimeSortTypes, List<Anime>> = linkedMapOf()
 )
