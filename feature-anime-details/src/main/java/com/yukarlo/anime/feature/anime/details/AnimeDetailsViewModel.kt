0c655b20233dab0c015290a59437a0c5a4aea835
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-03 16:32:50 +0100

    add use case to view model and the fetch logic

--- feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsViewModel.kt
+++ feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsViewModel.kt
@@ -1,12 +1,42 @@
 package com.yukarlo.anime.feature.anime.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yukarlo.anime.common.android.base.Result
+import com.yukarlo.anime.lib.anime.domain.GetAnimeDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class AnimeDetailsViewModel @Inject constructor(
-
+    private val animeDetailsUseCase: GetAnimeDetailsUseCase
 ) : ViewModel() {
 
+    private val animeDetails: MutableStateFlow<AnimeDetailsUiState> =
+        MutableStateFlow(AnimeDetailsUiState())
+    val onAnimeDetails: StateFlow<AnimeDetailsUiState> = animeDetails
+
+    fun getAnimeDetails(animeId: Int) {
+        viewModelScope.launch {
+            animeDetailsUseCase.execute(param = animeId)
+                .onStart {
+                    animeDetails.value = AnimeDetailsUiState(
+                        result = Result.LOADING
+                    )
+                }
+                .catch {
+                    animeDetails.value = AnimeDetailsUiState(
+                        animeDetails = animeDetails.value.animeDetails
+                    )
+                }
+                .collect {
+                    animeDetails.value = AnimeDetailsUiState(
+                        result = Result.SUCCESS,
+                        animeDetails = it
+                    )
+                }
+        }
+    }
 }
