9852b67373253d3a2a6bb64c0b8adb39253bee64
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-03 15:27:24 +0100

    add anime details screen

---
+++ feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsScreen.kt
@@ -0,0 +1,14 @@
+package com.yukarlo.anime.feature.anime.details
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+
+@Composable
+fun AnimeDetailsScreen(
+    animeId: Int,
+    navBackStackEntry: NavBackStackEntry,
+    navController: NavController,
+) {
+
+}
