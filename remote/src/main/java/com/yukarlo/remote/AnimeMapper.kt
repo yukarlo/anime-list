d87cb6c497fc9dda25c710ae2a551468bc70a5cc
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-03 17:13:23 +0100

    simplified anime mapper

--- remote/src/main/java/com/yukarlo/remote/AnimeMapper.kt
+++ remote/src/main/java/com/yukarlo/remote/AnimeMapper.kt
@@ -11,149 +11,50 @@ internal class AnimeMapper @Inject constructor() {
 
     fun mapAnimeToDomain(result: List<AnimeQuery.Medium?>?): List<Anime>? =
         result?.map {
-            mapAnime(
-                id = it?.fragments?.animeMedia?.id,
-                title = Title(
-                    english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                    native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                    userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                ),
-                image = Image(
-                    extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                    large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                ),
-                genres = it?.fragments?.animeMedia?.genres,
-                status = it?.fragments?.animeMedia?.status?.name,
-                startDate = it?.fragments?.animeMedia?.startDate,
-                endDate = it?.fragments?.animeMedia?.endDate,
-                format = it?.fragments?.animeMedia?.format?.name
-            )
+            mapAnime(animeMedia = it?.fragments?.animeMedia)
         }
 
     fun mapMultipleAnimeToDomain(data: MultipleAnimeSortQuery.Data): MultipleAnimeSort =
         MultipleAnimeSort(
             top10 = data.top10?.media?.map {
-                mapAnime(
-                    id = it?.fragments?.animeMedia?.id,
-                    title = Title(
-                        english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                        native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                        userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                    ),
-                    image = Image(
-                        extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                        large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                    ),
-                    genres = it?.fragments?.animeMedia?.genres,
-                    status = it?.fragments?.animeMedia?.status?.name,
-                    startDate = it?.fragments?.animeMedia?.startDate,
-                    endDate = it?.fragments?.animeMedia?.endDate,
-                    format = it?.fragments?.animeMedia?.format?.name
-                )
+                mapAnime(animeMedia = it?.fragments?.animeMedia)
             } ?: emptyList(),
             popularThisSeason = data.popularThisSeason?.media?.map {
-                mapAnime(
-                    id = it?.fragments?.animeMedia?.id,
-                    title = Title(
-                        english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                        native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                        userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                    ),
-                    image = Image(
-                        extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                        large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                    ),
-                    genres = it?.fragments?.animeMedia?.genres,
-                    status = it?.fragments?.animeMedia?.status?.name,
-                    startDate = it?.fragments?.animeMedia?.startDate,
-                    endDate = it?.fragments?.animeMedia?.endDate,
-                    format = it?.fragments?.animeMedia?.format?.name
-                )
+                mapAnime(animeMedia = it?.fragments?.animeMedia)
             } ?: emptyList(),
             trendingNow = data.trendingNow?.media?.map {
-                mapAnime(
-                    id = it?.fragments?.animeMedia?.id,
-                    title = Title(
-                        english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                        native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                        userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                    ),
-                    image = Image(
-                        extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                        large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                    ),
-                    genres = it?.fragments?.animeMedia?.genres,
-                    status = it?.fragments?.animeMedia?.status?.name,
-                    startDate = it?.fragments?.animeMedia?.startDate,
-                    endDate = it?.fragments?.animeMedia?.endDate,
-                    format = it?.fragments?.animeMedia?.format?.name
-                )
+                mapAnime(animeMedia = it?.fragments?.animeMedia)
             } ?: emptyList(),
             allTimePopular = data.allTimePopular?.media?.map {
-                mapAnime(
-                    id = it?.fragments?.animeMedia?.id,
-                    title = Title(
-                        english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                        native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                        userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                    ),
-                    image = Image(
-                        extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                        large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                    ),
-                    genres = it?.fragments?.animeMedia?.genres,
-                    status = it?.fragments?.animeMedia?.status?.name,
-                    startDate = it?.fragments?.animeMedia?.startDate,
-                    endDate = it?.fragments?.animeMedia?.endDate,
-                    format = it?.fragments?.animeMedia?.format?.name
-                )
+                mapAnime(animeMedia = it?.fragments?.animeMedia)
             } ?: emptyList()
         )
 
     fun mapAnimeDetailsToDomain(result: AnimeDetailsQuery.Media?): AnimeDetails =
-        result.let {
-            AnimeDetails(
-                basicInfo = mapAnime(
-                    id = null,
-                    Title(
-                        english = it?.fragments?.animeMedia?.title?.english.orEmpty(),
-                        native = it?.fragments?.animeMedia?.title?.native_.orEmpty(),
-                        userPreferred = it?.fragments?.animeMedia?.title?.userPreferred.orEmpty()
-                    ),
-                    image = Image(
-                        extraLarge = it?.fragments?.animeMedia?.coverImage?.extraLarge.orEmpty(),
-                        large = it?.fragments?.animeMedia?.coverImage?.large.orEmpty()
-                    ),
-                    genres = it?.fragments?.animeMedia?.genres,
-                    status = it?.fragments?.animeMedia?.status?.name,
-                    startDate = it?.fragments?.animeMedia?.startDate,
-                    endDate = it?.fragments?.animeMedia?.endDate,
-                    format = it?.fragments?.animeMedia?.format?.name
-                )
-            )
-        }
+        AnimeDetails(
+            basicInfo = mapAnime(animeMedia = result?.fragments?.animeMedia)
+        )
 
-    private fun mapAnime(
-        id: Int?,
-        title: Title,
-        image: Image,
-        status: String?,
-        genres: List<String?>?,
-        startDate: AnimeMedia.StartDate?,
-        endDate: AnimeMedia.EndDate?,
-        format: String?
-    ): Anime = Anime(
-        id = id,
-        title = title,
-        coverImage = image,
-        status = status.orEmpty(),
-        genres = genres.orEmpty().joinToString(separator = " • "),
-        startDate = startDate?.let {
-            "${it.month}.${it.day}.${it.year}"
-        } ?: "",
-        endDate = endDate?.let {
-            "${it.month}.${it.day}.${it.year}"
-        } ?: "",
-        formatAndYear = "$format • ${startDate?.year}"
-    )
+    private fun mapAnime(animeMedia: AnimeMedia?): Anime =
+        Anime(
+            id = animeMedia?.id,
+            title = Title(
+                english = animeMedia?.title?.english.orEmpty(),
+                userPreferred = animeMedia?.title?.userPreferred.orEmpty(),
+                native = animeMedia?.title?.native_.orEmpty(),
+            ),
+            coverImage = Image(
+                extraLarge = animeMedia?.coverImage?.extraLarge.orEmpty(),
+                large = animeMedia?.coverImage?.large.orEmpty()
+            ),
+            status = animeMedia?.status?.name.orEmpty(),
+            genres = animeMedia?.genres.orEmpty().joinToString(separator = " • "),
+            startDate = animeMedia?.startDate?.let {
+                "${it.month}.${it.day}.${it.year}"
+            } ?: "",
+            endDate = animeMedia?.endDate?.let {
+                "${it.month}.${it.day}.${it.year}"
+            } ?: "",
+            formatAndYear = "${animeMedia?.format?.name?.toLowerCase()} • ${animeMedia?.startDate?.year}"
+        )
 }
