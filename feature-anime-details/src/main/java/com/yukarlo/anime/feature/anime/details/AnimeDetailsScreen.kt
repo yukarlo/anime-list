22b0db2057660b96197f7e30e73a0abbe0abb6f0
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-03 15:30:10 +0100

    add anime details view model

--- feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsScreen.kt
+++ feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsScreen.kt
@@ -1,6 +1,9 @@
 package com.yukarlo.anime.feature.anime.details
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.HiltViewModelFactory
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 
@@ -10,5 +13,12 @@ fun AnimeDetailsScreen(
     navBackStackEntry: NavBackStackEntry,
     navController: NavController,
 ) {
-
+    val factory = HiltViewModelFactory(
+        context = LocalContext.current,
+        navBackStackEntry = navBackStackEntry
+    )
+    val viewModel: AnimeDetailsViewModel = viewModel(
+        key = AnimeDetailsViewModel::class.java.simpleName,
+        factory = factory
+    )
 }
---
+++ feature-anime-details/src/main/java/com/yukarlo/anime/feature/anime/details/AnimeDetailsViewModel.kt
@@ -0,0 +1,12 @@
+package com.yukarlo.anime.feature.anime.details
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+internal class AnimeDetailsViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
