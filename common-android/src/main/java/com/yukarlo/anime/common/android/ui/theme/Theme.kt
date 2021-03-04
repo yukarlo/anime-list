88c123698623ee9849987f9c92e7428aaa5eb7c0
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-03-04 15:27:36 +0100

    add new theme colour

--- common-android/src/main/java/com/yukarlo/anime/common/android/ui/theme/Theme.kt
+++ common-android/src/main/java/com/yukarlo/anime/common/android/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.yukarlo.anime.common.android.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -10,10 +11,11 @@ private val DarkColorPalette = darkColors(
     primary = grey900,
     primaryVariant = grey900,
     secondary = teal200,
+    secondaryVariant = yellow300,
     background = grey1000,
     surface = grey900,
     onPrimary = grey50,
-    onSecondary = grey50,
+    onSecondary = grey800,
     onBackground = grey50,
     onSurface = grey50
 )
@@ -22,22 +24,57 @@ private val LightColorPalette = lightColors(
     primary = grey200,
     primaryVariant = grey700,
     secondary = teal200,
+    secondaryVariant = yellow800,
     background = grey50,
     surface = grey200,
     onPrimary = grey700,
-    onSecondary = grey200,
+    onSecondary = grey50,
     onBackground = grey900,
     onSurface = grey900
 )
 
 @Composable
-fun AnimeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun AnimeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
 
+    AppTheme(colors = colors, content = content)
+}
+
+private val GreyThemeLight = lightColors(
+    surface = grey200,
+    onPrimary = grey800,
+)
+
+private val GreyThemeDark = darkColors(
+    surface = grey900,
+    onPrimary = grey100,
+)
+
+@Composable
+fun GreyTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        GreyThemeDark
+    } else {
+        GreyThemeLight
+    }
+    AppTheme(colors = colors, content = content)
+}
+
+@Composable
+fun AppTheme(
+    colors: Colors,
+    content: @Composable() () -> Unit
+) {
     MaterialTheme(
         colors = colors,
         typography = typography,
