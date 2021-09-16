02de0eb1ecef0d3af60a922f751ef712bf58dfe5
Karlo Yu <karlo.miguel.p.yu@gmail.com>
2021-09-16 19:52:27 +0200

    bumo target and compile version

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,7 +1,7 @@
 object Android {
     object Sdk {
         const val MIN = 23
-        const val TARGET = 30
-        const val COMPILE = 30
+        const val TARGET = 31
+        const val COMPILE = 31
     }
 }
