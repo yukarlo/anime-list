c99d60f0b873c11d6dadfc79bd2d57b57f9352ed
Karlo Yu <karlo.miguel.p.yu@gmail.com>
2021-09-16 20:49:51 +0200

    rename to settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Anime"
+include(
+    ":app",
+    ":common-android",
+    ":feature-anime-home",
+    ":remote",
+    ":core-model",
+    ":lib-anime",
+    ":feature-anime-list",
+    ":feature-anime-details",
+    ":feature-anime-search",
+    ":feature-about"
+)
