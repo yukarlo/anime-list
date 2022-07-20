8ea5f9151e8ab4958374a77781eae80b5afb170b
Karlo Yu <karlo.miguel.p.yu@gmail.com>
2022-07-20 10:43:56 +0200

    create build-logic module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
